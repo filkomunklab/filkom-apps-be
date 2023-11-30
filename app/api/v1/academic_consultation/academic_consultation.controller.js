76191599c52cb80a4e14bbea9b53db5e62457dc2
jericokatong <jericokatong@gmail.com>
2023-11-30 15:27:04 +0800

    feat: skema tabel academic_consultation_id and message create consultation service get consultation by nim service get consultation by nik service get detail consultation by id service update status consultation complete service get message by consultation id service create message service

--- app/api/v1/academic_consultation/academic_consultation.controller.js
+++ app/api/v1/academic_consultation/academic_consultation.controller.js
@@ -1,3 +1,70 @@
 const academicConsService = require("./academic_consultation.service");
 
-module.exports = {};
+const createConsultation = async (req, res) => {
+  const payload = req.body;
+  try {
+    const consultation = await academicConsService.createConsultation(payload);
+    res.status(201).send({ status: "OK", data: consultation });
+  } catch (error) {
+    res
+      .status(error?.status || 500)
+      .send({ status: "FAILED", data: { error: error?.message || error } });
+  }
+};
+
+const getConsultationByNim = async (req, res) => {
+  const nim = req.params.nim;
+  try {
+    const consultation = await academicConsService.getConsultationByNim(nim);
+    res.status(200).send({ status: "OK", data: consultation });
+  } catch (error) {
+    res
+      .status(error?.status || 500)
+      .send({ status: "FAILED", data: { error: error?.message || error } });
+  }
+};
+
+const getConsultationByNik = async (req, res) => {
+  const nik = req.params.nik;
+  try {
+    const consultation = await academicConsService.getConsultationByNik(nik);
+    res.status(200).send({ status: "OK", data: consultation });
+  } catch (error) {
+    res
+      .status(error?.status || 500)
+      .send({ status: "FAILED", data: { error: error?.message || error } });
+  }
+};
+
+const getConsultationById = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const consultation = await academicConsService.getConsultationById(id);
+    res.status(200).send({ status: "OK", data: consultation });
+  } catch (error) {
+    res
+      .status(error?.status || 500)
+      .send({ status: "FAILED", data: { error: error?.message || error } });
+  }
+};
+
+const updateConsultationStatusCompleteById = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const consultation =
+      await academicConsService.updateConsultationStatusCompleteById(id);
+    res.status(200).send({ status: "OK", data: consultation });
+  } catch (error) {
+    res
+      .status(error?.status || 500)
+      .send({ status: "FAILED", data: { error: error?.message || error } });
+  }
+};
+
+module.exports = {
+  createConsultation,
+  getConsultationByNim,
+  getConsultationByNik,
+  getConsultationById,
+  updateConsultationStatusCompleteById,
+};
--- app/api/v1/academic_consultation/academic_consultation.service.js
+++ app/api/v1/academic_consultation/academic_consultation.service.js
@@ -1,3 +1,107 @@
 const academicConsRepository = require("./academic_consultation.repository");
+const moment = require("moment-timezone");
 
-module.exports = {};
+const createConsultation = async (payload) => {
+  try {
+    const consultation = await academicConsRepository.insertConsultation(
+      payload
+    );
+
+    return {
+      ...consultation,
+      createdAt: `${moment(consultation.createdAt).tz("Asia/Makassar")}`,
+      updatedAt: `${moment(consultation.updatedAt).tz("Asia/Makassar")}`,
+    };
+  } catch (error) {
+    throw error.message;
+  }
+};
+
+const getConsultationByNim = async (nim) => {
+  try {
+    let consultation = await academicConsRepository.selectConsultationByNim(
+      nim
+    );
+
+    consultation = consultation
+      ? consultation.map((value) => {
+          return {
+            ...value,
+            createdAt: `${moment(value.createdAt).tz("Asia/Makassar")}`,
+            updatedAt: `${moment(value.updatedAt).tz("Asia/Makassar")}`,
+          };
+        })
+      : [];
+
+    return consultation;
+  } catch (error) {
+    throw error.message;
+  }
+};
+
+const getConsultationByNik = async (nik) => {
+  try {
+    let consultation = await academicConsRepository.selectConsultationByNik(
+      nik
+    );
+
+    consultation = consultation
+      ? consultation.map((value) => {
+          return {
+            ...value,
+            createdAt: `${moment(value.createdAt).tz("Asia/Makassar")}`,
+            updatedAt: `${moment(value.updatedAt).tz("Asia/Makassar")}`,
+          };
+        })
+      : [];
+    return consultation;
+  } catch (error) {
+    throw error.message;
+  }
+};
+
+const getConsultationById = async (id) => {
+  try {
+    const consultation = await academicConsRepository.selectConsultationById(
+      id
+    );
+
+    return consultation
+      ? {
+          ...consultation,
+          createdAt: `${moment(consultation.createdAt).tz("Asia/Makassar")}`,
+          updatedAt: `${moment(consultation.updatedAt).tz("Asia/Makassar")}`,
+        }
+      : {};
+  } catch (error) {
+    throw error.message;
+  }
+};
+
+const updateConsultationStatusCompleteById = async (id) => {
+  const NOW = moment.tz("Asia/Makassar");
+  try {
+    let consultation =
+      await academicConsRepository.updateStatusConsultationById(
+        id,
+        "Complete",
+        NOW
+      );
+
+    return {
+      ...consultation,
+      createdAt: `${moment(consultation.createdAt).tz("Asia/Makassar")}`,
+      updatedAt: `${moment(consultation.updatedAt).tz("Asia/Makassar")}`,
+    };
+  } catch (error) {
+    throw error.message;
+  }
+};
+
+module.exports = {
+  createConsultation,
+  getConsultationByNim,
+  getConsultationByNik,
+  getConsultationById,
+  updateConsultationStatusCompleteById,
+};
