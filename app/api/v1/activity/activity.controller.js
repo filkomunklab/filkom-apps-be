6d14f6f1142fe1244cc1006c76a5a46f7ff2f996
Alden <lcomputer04@gmail.com>
2023-12-04 10:21:56 +0800

    feat: insert grades and open approval

--- app/api/v1/activity/activity.controller.js
+++ app/api/v1/activity/activity.controller.js
@@ -28,7 +28,21 @@ const detailActivity = async (req, res) => {
   }
 };
 
+const gradesSubmission = async (req, res) => {
+  const payload = req.body;
+  const { nim } = req.params;
+  try {
+    const activity = await activityService.viewDetailActivity(payload, nim);
+    res.status(201).send({ status: "OK", data: activity });
+  } catch (error) {
+    res
+      .status(error?.status || 500)
+      .send({ status: "FAILED", data: { error: error?.message || error } });
+  }
+};
+
 module.exports = {
   crateActivity,
   detailActivity,
+  gradesSubmission,
 };
