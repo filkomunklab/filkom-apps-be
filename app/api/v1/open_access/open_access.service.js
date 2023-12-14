671209bf51861a173ee6b2b9c257a1daceb96cb3
Alden <lcomputer04@gmail.com>
2023-12-14 17:06:25 +0800

    feat: get All List of Grade Access

--- app/api/v1/open_access/open_access.service.js
+++ app/api/v1/open_access/open_access.service.js
@@ -1,22 +1,5 @@
 const accessRepository = require("./open_access.repository");
 
-// const createOpenAccessTodb = async (payload) => {
-//   try {
-//     const openAccess = await openRepository.addOpenAccessTodb(payload);
-//     return openAccess;
-//   } catch (error) {
-//     throw error;
-//   }
-// };
-
-// const patchSetOpenAccess = async (id, set_access) => {
-//   try {
-//     return openRepository.changeSetAccess(id, set_access);
-//   } catch (error) {
-//     throw error;
-//   }
-// };
-
 const createOpenGradAccess = async (payload) => {
   try {
     const openAccess = await accessRepository.addOpenGradesAccess(payload);
@@ -35,9 +18,17 @@ const setGradesAccessClose = async (id, isOpen) => {
   }
 };
 
+const viewlistGradeAccess = async () => {
+  try {
+    const openAccess = await accessRepository.findlistGradesAccess();
+    return openAccess;
+  } catch (error) {
+    throw error;
+  }
+};
+
 module.exports = {
-  //   createOpenAccessTodb,
-  //   patchSetOpenAccess,
   createOpenGradAccess,
   setGradesAccessClose,
+  viewlistGradeAccess,
 };
