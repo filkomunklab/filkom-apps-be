135a3b9f9b58be5ba13f09c1d45026c814feab65
Darell Mona <s2200064@student.unklab.ac.id>
2024-01-15 07:22:31 +0800

    feat: activity policy

--- app/api/v1/activity/activity.controller.js
+++ app/api/v1/activity/activity.controller.js
@@ -1,9 +1,14 @@
 const activityService = require("./activity.service");
+const { activityPolicy } = require("./activiy.policy");
 
 //==========================DOSEN ACCESS=============================//
 const createActivity = async (req, res) => {
   const payload = req.body;
+  const policy = activityPolicy(req.user);
   try {
+    if (!policy.can("create", "activity")) {
+      throw { status: 403, message: "Forbidden Access" };
+    }
     const activity = await activityService.createActivity(payload);
     res.status(201).send({ status: "OK", data: activity });
   } catch (error) {
@@ -25,7 +30,11 @@ const takeAttendance = async (req, res) => {
     activityId,
     ...body,
   };
+  const policy = activityPolicy(req.user);
   try {
+    if (!policy.can("update", "attendance")) {
+      throw { status: 403, message: "Forbidden Access" };
+    }
     const activity = await activityService.takeAttendance(payload);
     res.status(200).send({ status: "OK", data: activity });
   } catch (error) {
@@ -42,7 +51,11 @@ const takeAttendance = async (req, res) => {
 
 const getStudentList = async (req, res) => {
   const payload = req.query;
+  const policy = activityPolicy(req.user);
   try {
+    if (!policy.can("read", "studentList")) {
+      throw { status: 403, message: "Forbidden Access" };
+    }
     const studentList = await activityService.getStudentList(payload);
     res.status(200).send({ status: "OK", data: studentList });
   } catch (error) {
@@ -59,7 +72,11 @@ const getStudentList = async (req, res) => {
 
 const getHistoryForAdvisor = async (req, res) => {
   const payload = req.params;
+  const policy = activityPolicy(req.user);
   try {
+    if (!policy.can("read", "historyForAdvisor")) {
+      throw { status: 403, message: "Forbidden Access" };
+    }
     const history = await activityService.getHistoryForAdvisor(payload);
     res.status(200).send({ status: "OK", data: history });
   } catch (error) {
@@ -72,7 +89,11 @@ const getHistoryForAdvisor = async (req, res) => {
 //==========================Student Access===========================//
 const getHistoryForStudent = async (req, res) => {
   const payload = req.params;
+  const policy = activityPolicy(req.user);
   try {
+    if (!policy.can("read", "historyForStudent")) {
+      throw { status: 403, message: "Forbidden Access" };
+    }
     const history = await activityService.getHistoryForStudent(payload);
     res.status(200).send({ status: "OK", data: history });
   } catch (error) {
@@ -85,7 +106,11 @@ const getHistoryForStudent = async (req, res) => {
 //==============================GENERAL ACCESS=======================//
 const detailActivity = async (req, res) => {
   const payload = req.params;
+  const policy = activityPolicy(req.user);
   try {
+    if (!policy.can("read", "Activity")) {
+      throw { status: 403, message: "Forbidden Access" };
+    }
     const activity = await activityService.viewDetailActivity(payload);
     res.status(201).send({ status: "OK", data: activity });
   } catch (error) {
@@ -98,7 +123,11 @@ const detailActivity = async (req, res) => {
 
 const getRecentActivity = async (req, res) => {
   const payload = req.params;
+  const policy = activityPolicy(req.user);
   try {
+    if (!policy.can("read", "currentActivity")) {
+      throw { status: 403, message: "Forbidden Access" };
+    }
     const recentActivity = await activityService.getRecentActivity(payload);
     res.status(200).send({ status: "OK", data: recentActivity });
   } catch (error) {
--- app/api/v1/activity/activity.routes.js
+++ app/api/v1/activity/activity.routes.js
@@ -1,27 +1,35 @@
 const express = require("express");
 const activityController = require("./activity.controller");
 const router = express.Router();
+const { auth } = require("../../../middleware/auth");
 
-router.post("/activity", activityController.createActivity);
+router.post("/activity", auth, activityController.createActivity);
 
-router.get("/activity/detail/:activityId", activityController.detailActivity);
+router.get(
+  "/activity/detail/:activityId",
+  auth,
+  activityController.detailActivity
+);
 
-router.get("/activity/student-list", activityController.getStudentList);
+router.get("/activity/student-list", auth, activityController.getStudentList);
 
 router.get(
   "/activity/history-for-student/:studentNim",
+  auth,
   activityController.getHistoryForStudent
 );
 
 router.get(
   "/activity/history-for-advisor/:employeeNik",
+  auth,
   activityController.getHistoryForAdvisor
 );
 
-router.get("/activity/current/:id", activityController.getRecentActivity);
+router.get("/activity/current/:id", auth, activityController.getRecentActivity);
 
 router.patch(
   "/activity/take-attendance/:activityId",
+  auth,
   activityController.takeAttendance
 );
 
