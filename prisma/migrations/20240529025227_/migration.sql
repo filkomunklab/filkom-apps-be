77568bbe1f5a2da0e061383f8cd62f4b105903d8
Darell Mona <s2200064@student.unklab.ac.id>
2024-05-29 12:22:26 +0800

    fix: remove gradingSystem from ReportDetail

---
+++ prisma/migrations/20240529025227_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `gradingSystem` on the `ReportDetail` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "ReportDetail" DROP COLUMN "gradingSystem";
